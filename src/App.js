20cd5f402835550d859c804ce361aabfe6cd9c79
Liam <kingliam28@ntlworld.com>
2019-03-04 13:34:20 +0000

    Update animations and fix ghost card

--- src/App.js
+++ src/App.js
@@ -12,15 +12,17 @@ const Project = ({ title, description, image, link, opacity }) => (
   <a className='projectLink' href={link}>
 
     <Flip left duration={1500}>
-      <div className="project-summary" style={{ opacity: opacity }}>
+      <div style={{ opacity: opacity }}>
+        <div className="project-summary" style={{ opacity: opacity }}>
 
-        <div className='projectDivImage'>
-          <img className="projectImage" src={image}></img>
-        </div>
-        <div className='projectDiv'>
-          <div className='projectText'>
-            <h2 className="projectTitle">{title}</h2>
-            <h4 className="projectDescription">{description}</h4>
+          <div className='projectDivImage'>
+            <img className="projectImage" src={image}></img>
+          </div>
+          <div className='projectDiv'>
+            <div className='projectText'>
+              <h2 className="projectTitle">{title}</h2>
+              <h4 className="projectDescription">{description}</h4>
+            </div>
           </div>
         </div>
       </div>
@@ -188,14 +190,14 @@ const Header = (name) => (
 
   <div className='header'>
     <div>
-      <Flip left>
+      <Flip left delay={1000}>
         <div className='headerImage'>
           <img className='center imgCircle' src='./images/myAvatar'></img>
 
         </div>
       </Flip>
 
-      <Fade>
+      <Fade delay={800}>
         <h1 className='textCenter titleText'>Liam D. Eloie</h1>
         <p className='textCenter'>liam.d.eloie@gmail.com</p>
 
@@ -221,16 +223,24 @@ const About = () => (
     <h1 className='aboutMeTitle'>
       About Me
     </h1>
-    <Fade duration={1500}>
-      <hr className='hrStyleBlue'></hr>
+    <hr className='hrStyleBlue'></hr>
+
+    <Fade left duration={1500}>
       <p className='aboutMeDescription'>
         I am a recent master's graduate in both Physics and Machine Learning at University College London (UCL).
     </p>
+    </Fade>
+
+    <Fade right duration={1500}>
       <p className='aboutMeDescription'>
         Throughout my time at university, I have acquired a great thirst for knowledge, as well as powerful tools, required for discovering truths about our world. I am extremely passionate about using my skills and expertise to have a positive impact on society and to make the world a better place.
     </p>
-      <p className='aboutMeDescription'> During my master's in Physics, I was fortunate enough to work alongside world leading experts in quantum computing, simulating quantum logic gates in a system of particles. This project introduced me to the field of machine learning, applied to quantum computing. Specifically, I used evolutionary algorithms to optimise the interactions between a system of particles to make it perform certain logical operations. I found these tools to be incredibly powerful, opening up a whole new world of discovery, which sparked my initial interest in machine learning. </p>
+    </Fade>
 
+    <Fade left duration={1500}>
+      <p className='aboutMeDescription'> During my master's in Physics, I was fortunate enough to work alongside world leading experts in quantum computing, simulating quantum logic gates in a system of particles. This project introduced me to the field of machine learning, applied to quantum computing. Specifically, I used evolutionary algorithms to optimise the interactions between a system of particles to make it perform certain logical operations. I found these tools to be incredibly powerful, opening up a whole new world of discovery, which sparked my initial interest in machine learning. </p>
+    </Fade>
+    <Fade right duration={1500}>
       <p className='aboutMeDescription'> As a result, I decided to pursue a MSc Machine Learning degree at UCL, where I was fortunately awarded a fully-funded scholarship from Google DeepMind. Not only did I learn the foundations of machine learning through this degree, but I was also introduced to state-of-the-art techniques within deep learning and reinforcement learning. Using these newly found skills, I undertook a master's project at the Francis Crick Institution in the field of protein function prediction.  </p>
     </Fade>
   </div>
