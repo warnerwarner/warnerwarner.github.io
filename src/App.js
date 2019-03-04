bd20751a41e999c18cd0c96fe246bbbe8794fdb5
Liam <kingliam28@ntlworld.com>
2019-03-04 14:11:54 +0000

    Animate about me

--- src/App.js
+++ src/App.js
@@ -225,27 +225,27 @@ const Header = (name) => (
 const About = () => (
   <div className='aboutMe' id='aboutMe' aos-duration="500">
 
-    <h1 className='aboutMeTitle'>
-      About Me
+    <Fade left>
+      <h1 className='aboutMeTitle'>
+        About Me
     </h1>
-    <hr className='hrStyleBlue'></hr>
+      <hr className='hrStyleBlue'></hr>
 
-    <Fade left duration={1500}>
       <p className='aboutMeDescription'>
         I am a recent master's graduate in both Physics and Machine Learning at University College London (UCL).
     </p>
     </Fade>
 
-    <Fade right duration={1500}>
+    <Fade right>
       <p className='aboutMeDescription'>
         Throughout my time at university, I have acquired a great thirst for knowledge, as well as powerful tools, required for discovering truths about our world. I am extremely passionate about using my skills and expertise to have a positive impact on society and to make the world a better place.
     </p>
     </Fade>
 
-    <Fade left duration={1500}>
+    <Fade left>
       <p className='aboutMeDescription'> During my master's in Physics, I was fortunate enough to work alongside world leading experts in quantum computing, simulating quantum logic gates in a system of particles. This project introduced me to the field of machine learning, applied to quantum computing. Specifically, I used evolutionary algorithms to optimise the interactions between a system of particles to make it perform certain logical operations. I found these tools to be incredibly powerful, opening up a whole new world of discovery, which sparked my initial interest in machine learning. </p>
     </Fade>
-    <Fade right duration={1500}>
+    <Fade right>
       <p className='aboutMeDescription'> As a result, I decided to pursue a MSc Machine Learning degree at UCL, where I was fortunately awarded a fully-funded scholarship from Google DeepMind. Not only did I learn the foundations of machine learning through this degree, but I was also introduced to state-of-the-art techniques within deep learning and reinforcement learning. Using these newly found skills, I undertook a master's project at the Francis Crick Institution in the field of protein function prediction.  </p>
     </Fade>
   </div>
